c27e04686027b4665589769485b1456384d4ff42
PastelQ <a0941@naver.com>
2024-08-11 13:09:35 +0900

    2024/08/11 / Java 시작하기

---
+++ java/src/chap01/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,12 @@
+package chap01.sec03.exam02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+
+		int x = 1;					// 변수 x 선언 및 1을 저장
+		int y = 2;					// 변수 y 선언 및 2를 저장
+		int result = x + y;			// 변수 result 선언 및 x + y 값을 저장
+		System.out.println(result); // 모니터에 출력되는 메서드 호출
+	}
+}
