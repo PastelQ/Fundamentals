8565c30b8103af7dd79241ca04e6f9d3be953a9a
PastelQ <a0941@naver.com>
2024-08-20 23:27:12 +0900

    2024/08/20 / import문 사용법 자세히

---
+++ java/src/chap06_class/sec06_packageAndAccessModifier/sec01_package/exam02_import/hankook/SnowTire.java
@@ -0,0 +1,3 @@
+package chap06_class.sec06_packageAndAccessModifier.sec01_package.exam02_import.hankook;
+// hankook 패키지
+public class SnowTire {}
---
+++ java/src/chap06_class/sec06_packageAndAccessModifier/sec01_package/exam02_import/kumho/Tire.java
@@ -0,0 +1,3 @@
+package chap06_class.sec06_packageAndAccessModifier.sec01_package.exam02_import.kumho;
+// kumho 패키지
+public class Tire {}
