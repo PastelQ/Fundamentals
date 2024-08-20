f1ac4ff6c02efa5180df8a1bed392b3715020c1f
PastelQ <a0941@naver.com>
2024-08-21 00:09:21 +0900

    2024/08/21 / 생성자 접근제한자 확인

--- java/src/chap06_class/sec06_packageAndAccessModifier/exam03_classAccessModifier/package1/A.java
+++ java/src/chap06_class/sec06_packageAndAccessModifier/exam03_classAccessModifier/package1/A.java
@@ -1,6 +1,7 @@
 package chap06_class.sec06_packageAndAccessModifier.exam03_classAccessModifier.package1;
 
-// default 접근 제한
+// Access Modifier 사용 이유 : 외부로부터 데이터를 보호하기 위함
+// 외부엔 불필요하고 내부적으로만 사용 혹은 그런 부분을 은닉하기 위해(보안 목적)
 class A {}
 
 /* Access Modifier(접근 제한자) 종류
