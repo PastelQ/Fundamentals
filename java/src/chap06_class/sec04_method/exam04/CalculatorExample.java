407d6c9217792145d6934c158f25edff2ce437cf
PastelQ <a0941@naver.com>
2024-08-20 13:45:54 +0900

    2024/08/20 / 클래스 내부에서 메서드 호출, 메서드 연결

---
+++ java/src/chap06_class/sec04_method/exam04/CalculatorExample.java
@@ -0,0 +1,13 @@
+package chap06_class.sec04_method.exam04;
+
+// 실행 메서드 execute()를 실행
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		// 객체 생성
+		Calculator cal = new Calculator();
+		
+		// 실행 메서드
+		cal.execute();
+	}
+}
