f09066b76a57371e2df089b75f7abb843fd47ae9
PastelQ <a0941@naver.com>
2024-08-21 21:43:25 +0900

    2024/08/21 / 각 Game에 Interface(GamingConsole)를 도입하여 느슨한 조합의 흐름 알아보기

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasic.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasic.java
@@ -2,15 +2,18 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasic {
 	// 실행 메서드
 	public static void main(String[] args) {
 		
-		//var marioGame = new MarioGame();
-		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(superContraGame);
+		//var game = new MarioGame();
+		//var game = new SuperContraGame();
+		var game = new PacmanGame();
+		
+		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
 }
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GamingConsole.java
@@ -0,0 +1,10 @@
+package com.in28minutes.learnspringframework.game;
+
+// 각 게임들이 구현할 인터페이스
+public interface GamingConsole {
+	// 구현해야할 메서드가 정의되어 있음
+	void up();
+	void down();
+	void left();
+	void right();
+}
