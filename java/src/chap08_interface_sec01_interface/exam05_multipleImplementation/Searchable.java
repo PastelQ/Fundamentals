15fa89fca93e19152b035ee1220fb48f38628ae3
PastelQ <a0941@naver.com>
2024-08-23 19:08:11 +0900

    2024/08/23 / 다중 인터페이스 구현 클래스

---
+++ java/src/chap08_interface_sec01_interface/exam05_multipleImplementation/Searchable.java
@@ -0,0 +1,9 @@
+package chap08_interface_sec01_interface.exam05_multipleImplementation;
+
+// 구현 객체는 다수의 인터페이스 타입으로 사용 가능
+// 해당 객체는 다수의 인터페이스들에 선언된 추상 메서드를 구현하는 실체 메서드를 모두 선언해야함 
+public interface Searchable {
+
+	// method (public abstract)
+	void search(String url);
+}
