6b9aff1cb3abc250d476d049fe9f4f4af228371d
PastelQ <a0941@naver.com>
2024-08-24 20:25:31 +0900

    2024/08/24 / 코드 간소화 : Game package 및 App03GamingSpringBeans
    이전 프로젝트의 실행 클래스와 설정 클래스를 합쳐 하나의 클래스로 변경
    "learn-spring-framework-01" 프로젝트
    - App03GamingBasic.java
    - GamingConfiguration.java
    
    "learn-spring-framework-02" 프로젝트
    - App03GamingSpringBeans.java

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -1,33 +1,36 @@
 package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
-
+import com.in28minutes.learnspringframework.game.PacmanGame;
 
 @Configuration
-public class App03GamingBasic {
+public class App03GamingSpringBeans {
+	
+	@Bean
+	public GamingConsole game() {
+		var game = new PacmanGame();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
 	
 	public static void main(String[] args) {
 		
 		try(var context = 
 				new AnnotationConfigApplicationContext
-					(GamingConfiguration.class)){
+					(App03GamingSpringBeans.class)){
 			
 			context.getBean(GamingConsole.class).up();
 			context.getBean(GameRunner.class).run();
 		}
-		
-		
-					
-		
-		//var game = new MarioGame();
-		//var game = new SuperContraGame();
-//		var game = new PacmanGame();
-//		
-//		var gameRunner = new GameRunner(game);
-//		gameRunner.run();
 	}
 }
