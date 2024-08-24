a86c812b2f9c53ee715e4108fb99021d9a04ee48
PastelQ <a0941@naver.com>
2024-08-24 20:56:18 +0900

    2024/08/24 / @ComponentScan, @Component로 객체 생성
    Spring의 역할 추가
    1. 객체 관리 및 Auto-wiring 수행
    2. 객체 생성(여기선 @Component 사용)
    
    @Component : 클래스 위에 작성 시 Spring 컨테이너에 의해 자동으로 빈 등록
    (game.PacmanGame [6], GameRunner [5])
    @ComponentScan("컴포넌트가 명시된 클래스의 패키지") : 컴포넌트 스캔
    (App03GamingSpringBeans.java [12])

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -1,5 +1,9 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.stereotype.Component;
+
+// @Component : Spring 컨테이너에 의해 자동으로 빈으로 등록
+@Component
 public class PacmanGame implements GamingConsole {
 
 	public void up() {
