b5ff0a50550f6f21e9e0e0424ed26d6d197f6be9
PastelQ <a0941@naver.com>
2024-08-26 16:41:52 +0900

    2024/08/26 / 익명 부모 클래스, 구현 클래스 객체 생성 테스트

---
+++ java/src/chap09_nasted/sec02_anonymousObject/finalTest02/Vehicle.java
@@ -0,0 +1,9 @@
+package chap09_nasted.sec02_anonymousObject.finalTest02;
+
+/* AnonymousExample의 결과를 보고 Vehicle 인터페이스의 익명 구현 객체를
+  이용하여 필드, 로컬 변수의 초기값, 메서드 매개값을 대입 */
+/** 인터페이스 **/
+public interface Vehicle {
+	// 구현 필요 메서드 
+	public void run();
+}
