388673e466ab8f229e8375d84535711be1857b13
PastelQ <159614804+PastelQ@users.noreply.github.com>
2024-09-24 07:06:22 +0900

    2024/09/24 / 중복 java 디렉터리 삭제

--- java/src/chap07_inheritance/sec03_abstractClass/finalTest/HttpServlet.java
+++
@@ -1,8 +0,0 @@
-package chap07_inheritance.sec03_abstractClass.finalTest;
-
-// abstract class가 하기와 같이 선언
-public abstract class HttpServlet {
-
-	// abstract method
-	public abstract void service();
-}
