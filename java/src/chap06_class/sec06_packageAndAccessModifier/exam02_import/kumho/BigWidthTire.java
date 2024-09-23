a46ff730ebc3e7e3fcac8eca2eff6fc1d85e9677
PastelQ <159614804+PastelQ@users.noreply.github.com>
2024-09-24 06:29:58 +0900

    2024/09/24 / 중복 java 디렉터리 삭제

--- java/src/chap06_class/sec06_packageAndAccessModifier/exam02_import/kumho/BigWidthTire.java
+++
@@ -1,3 +0,0 @@
-package chap06_class.sec06_packageAndAccessModifier.exam02_import.kumho;
-// kumho 패키지
-public class BigWidthTire {}
--- java/src/chap07_inheritance/sec02_polymorphism/finalTest/SnowTire.java
+++
@@ -1,9 +0,0 @@
-package chap07_inheritance.sec02_polymorphism.finalTest;
-
-public class SnowTire extends Tire{
-
-	@Override
-	public void run() {
-		System.out.println("SnowTire가 굴러갑니다.");
-	}
-}
