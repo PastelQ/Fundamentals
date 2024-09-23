9a6b9ea9e4c33c70d2b670cde4613089267c0cfd
PastelQ <159614804+PastelQ@users.noreply.github.com>
2024-09-24 06:33:23 +0900

    2024/09/24 / 중복 java 디렉터리 삭제

--- java/src/chap07_inheritance/sec02_polymorphism/exam04_parameter/Taxi.java
+++
@@ -1,9 +0,0 @@
-package chap07_inheritance.sec02_polymorphism.exam04_parameter;
-
-// Vehicle 클래스를 상속 받는 하위 클래스
-public class Taxi extends Vehicle{
-
-	public void run() {
-		System.out.println("택시가 달립니다.");
-	}
-}
--- java/src/chap07_inheritance/sec03_abstractClass/finalTest/LoginServlet.java
+++
@@ -1,9 +0,0 @@
-package chap07_inheritance.sec03_abstractClass.finalTest;
-
-public class LoginServlet extends HttpServlet {
-
-	@Override
-	public void service() {
-		System.out.println("로그인 합니다.");
-	}
-}
