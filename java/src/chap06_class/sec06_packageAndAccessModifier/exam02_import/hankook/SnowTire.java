b472f0696697defc52a83be4560007f9d2b18a11
PastelQ <159614804+PastelQ@users.noreply.github.com>
2024-09-24 08:16:51 +0900

    2024/09/24 / 중복디렉터리

--- java/src/chap06_class/sec06_packageAndAccessModifier/exam02_import/hankook/SnowTire.java
+++
@@ -1,3 +0,0 @@
-package chap06_class.sec06_packageAndAccessModifier.exam02_import.hankook;
-// hankook 패키지
-public class SnowTire {}
--- java/src/chap08_interface/sec02_polymorphism/exam02_parameter/Vehicle.java
+++
@@ -1,8 +0,0 @@
-package chap08_interface.sec02_polymorphism.exam02_parameter;
-
-// 인터페이스(매개변수의 인터페이스화 공부)
-public interface Vehicle {
-
-	// 구현하는 클래스가 필수로 구현해야할 추상 메서드(public abstract)
-	public void run();
-}
